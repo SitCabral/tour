ca8c0c83f1fa3ffc34255fe10a807e76bad8f5c7
NOSI / NOSiGis / PM - Adilson Vaz Cabral <Adilson.Cabral@gov.cv>
2024-10-11 14:56:47 -0100

    first commit

---
+++ app-files/data.js
@@ -0,0 +1,111 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dscn0045",
+      "name": "DSCN0045",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": 0.20284645176166904,
+        "pitch": 0.47852266379917197,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9966587110018104,
+          "pitch": -0.02503020705047021,
+          "title": "Mindelo",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-dscn0050",
+      "name": "DSCN0050",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dscn0063",
+      "name": "DSCN0063",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
